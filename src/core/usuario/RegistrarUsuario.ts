aceca58567efc13605384d1d8e92a891a4974b82
CaioBritoDev <caio.freitas.brito@gmail.com>
2025-09-08 16:18:43 -0300

    Com melhores testes unitários

--- src/core/usuario/RegistrarUsuario.ts
+++ src/core/usuario/RegistrarUsuario.ts
@@ -1,10 +1,11 @@
 import type Banco from "../ports/Banco.js";
 import type Criptografia from "../ports/Criptografia.js";
+import type Usuario from "./Usuario.js";
 
 export default class RegistrarUsuario {
   constructor(private banco: Banco, private cripto: Criptografia) {}
 
-  async executar(usuario: any): Promise<void> {
+  async executar(usuario: Usuario): Promise<void> {
     if (!usuario.senha) throw new Error("Senha obrigatória");
     usuario.senha = await this.cripto.criptografar(usuario.senha);
     this.banco.salvar(usuario);
--- test/RegistrarUsuario.test.ts
+++ test/RegistrarUsuario.test.ts
@@ -5,30 +5,37 @@ import CriptografiaBcrypt from "../src/adapters/auth/CriptografiaBcrypt.js";
 import CriptografiaPlainText from "../src/adapters/auth/CriptografiaPlainText.js";
 
 import RegistrarUsuario from "../src/core/usuario/RegistrarUsuario.js";
+import type Usuario from "../src/core/usuario/Usuario.js";
 
 describe("RegistrarUsuario (banco em memória)", () => {
+  const criarUsuario = () =>
+    ({
+      nome: "Caio",
+      senha: "123456",
+      email: "caio@mail.com",
+    } as Usuario);
+
   test("Deve registar um usuário usando criptografia bcrypt", async () => {
-    const usuario = { nome: "Caio", senha: "123456" };
     const bancoEmMemoria = new BancoEmMemoria();
     const criptografiaBcrypt = new CriptografiaBcrypt();
     const casoDeUso = new RegistrarUsuario(bancoEmMemoria, criptografiaBcrypt);
-    await casoDeUso.executar(usuario);
+    await casoDeUso.executar(criarUsuario());
     expect(bancoEmMemoria.dados).toHaveLength(1);
-    expect(bancoEmMemoria.dados[0]).toEqual(usuario);
+    expect(bancoEmMemoria.dados[0]).toBeDefined();
   });
 
   test("Deve registrar um usuário usando criptografia plain text", async () => {
-    const usuario = { nome: "Caio", senha: "123456" };
     const bancoEmMemoria = new BancoEmMemoria();
     const criptografiaPlainText = new CriptografiaPlainText();
     const casoDeUso = new RegistrarUsuario(
       bancoEmMemoria,
       criptografiaPlainText
     );
+    const usuario = criarUsuario();
     await casoDeUso.executar(usuario);
     expect(bancoEmMemoria.dados).toHaveLength(1);
     expect(bancoEmMemoria.dados[0]).toEqual({
-      nome: "Caio",
+      ...usuario,
       senha: "hash-123456",
     });
   });
